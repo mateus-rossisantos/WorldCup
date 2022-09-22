101dfd61481ecc065d0dd9eb590eea249daab439
05220205 <rs482141>
2022-09-22 20:11:25 -0300

    entidades

--- src/model/WorldCup.java
+++
@@ -1,8 +0,0 @@
-package model;
-
-import jakarta.persistence.Entity;
-
-@Entity
-public class WorldCup {
-
-}
--- src/model/WorldCupMatches.java
+++ src/model/WorldCupMatches.java
@@ -1,8 +1,23 @@
 package model;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 
 @Entity
 public class WorldCupMatches {
-
+    @Id
+    private int matchId;
+    private int yearCup;
+    private String dateTime;
+    private String stage;
+    private String stadium;
+    private String city;
+    private String homeTeamName;
+    private String homeTeamGoals;
+    private String awayTeamName;
+    private String awayTeamGoals;
+    private String winConditions;
+    private String referee;
+    private String homeTeamInitials;
+    private String awayTeamInitials;
 }
