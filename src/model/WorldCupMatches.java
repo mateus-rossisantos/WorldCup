3b68e70a6bb3a39da494551b1b3f3860ee24f0b3
Edelberto Rosler <05220260@FARROUPILHA.IFRS.EDU.BR>
2022-09-22 20:24:27 -0300

    Create Getters and Setters

--- src/model/WorldCupMatches.java
+++ src/model/WorldCupMatches.java
@@ -20,4 +20,118 @@ public class WorldCupMatches {
     private String referee;
     private String homeTeamInitials;
     private String awayTeamInitials;
+
+
+
+    public int getMatchId() {
+        return matchId;
+    }
+
+    public void setMatchId(int matchId) {
+        this.matchId = matchId;
+    }
+
+    public int getYearCup() {
+        return yearCup;
+    }
+
+    public void setYearCup(int yearCup) {
+        this.yearCup = yearCup;
+    }
+
+    public String getDateTime() {
+        return dateTime;
+    }
+
+    public void setDateTime(String dateTime) {
+        this.dateTime = dateTime;
+    }
+
+    public String getStage() {
+        return stage;
+    }
+
+    public void setStage(String stage) {
+        this.stage = stage;
+    }
+
+    public String getStadium() {
+        return stadium;
+    }
+
+    public void setStadium(String stadium) {
+        this.stadium = stadium;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getHomeTeamName() {
+        return homeTeamName;
+    }
+
+    public void setHomeTeamName(String homeTeamName) {
+        this.homeTeamName = homeTeamName;
+    }
+
+    public String getHomeTeamGoals() {
+        return homeTeamGoals;
+    }
+
+    public void setHomeTeamGoals(String homeTeamGoals) {
+        this.homeTeamGoals = homeTeamGoals;
+    }
+
+    public String getAwayTeamName() {
+        return awayTeamName;
+    }
+
+    public void setAwayTeamName(String awayTeamName) {
+        this.awayTeamName = awayTeamName;
+    }
+
+    public String getAwayTeamGoals() {
+        return awayTeamGoals;
+    }
+
+    public void setAwayTeamGoals(String awayTeamGoals) {
+        this.awayTeamGoals = awayTeamGoals;
+    }
+
+    public String getWinConditions() {
+        return winConditions;
+    }
+
+    public void setWinConditions(String winConditions) {
+        this.winConditions = winConditions;
+    }
+
+    public String getReferee() {
+        return referee;
+    }
+
+    public void setReferee(String referee) {
+        this.referee = referee;
+    }
+
+    public String getHomeTeamInitials() {
+        return homeTeamInitials;
+    }
+
+    public void setHomeTeamInitials(String homeTeamInitials) {
+        this.homeTeamInitials = homeTeamInitials;
+    }
+
+    public String getAwayTeamInitials() {
+        return awayTeamInitials;
+    }
+
+    public void setAwayTeamInitials(String awayTeamInitials) {
+        this.awayTeamInitials = awayTeamInitials;
+    }
 }
--- src/model/WorldCups.java
+++ src/model/WorldCups.java
@@ -16,4 +16,86 @@ public class WorldCups {
     private int qualifiedTeams;
     private int matchesPlayed;
     private String attendance;
+
+
+    
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getWinner() {
+        return winner;
+    }
+
+    public void setWinner(String winner) {
+        this.winner = winner;
+    }
+
+    public String getRunnersUp() {
+        return runnersUp;
+    }
+
+    public void setRunnersUp(String runnersUp) {
+        this.runnersUp = runnersUp;
+    }
+
+    public String getThird() {
+        return third;
+    }
+
+    public void setThird(String third) {
+        this.third = third;
+    }
+
+    public String getFourth() {
+        return fourth;
+    }
+
+    public void setFourth(String fourth) {
+        this.fourth = fourth;
+    }
+
+    public int getGoalsScored() {
+        return goalsScored;
+    }
+
+    public void setGoalsScored(int goalsScored) {
+        this.goalsScored = goalsScored;
+    }
+
+    public int getQualifiedTeams() {
+        return qualifiedTeams;
+    }
+
+    public void setQualifiedTeams(int qualifiedTeams) {
+        this.qualifiedTeams = qualifiedTeams;
+    }
+
+    public int getMatchesPlayed() {
+        return matchesPlayed;
+    }
+
+    public void setMatchesPlayed(int matchesPlayed) {
+        this.matchesPlayed = matchesPlayed;
+    }
+
+    public String getAttendance() {
+        return attendance;
+    }
+
+    public void setAttendance(String attendance) {
+        this.attendance = attendance;
+    }
 }
